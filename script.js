0c74c4cd07fb8f3409f65558bf3943aa88f644d3
nikkehtine <nikkehtine@int.pl>
2023-04-25 15:56:56 +0200

    Create `script.js` Add variables for certain elements with IDs

---
+++ script.js
@@ -0,0 +1,16 @@
+// Form fields
+const nameField = document.getElementById("name");
+const numberField = document.getElementById("number");
+const monthField = document.getElementById("month");
+const yearField = document.getElementById("year");
+const cvvField = document.getElementById("cvv");
+
+// Buttons
+const confirmBtn = document.getElementById("confirm");
+const continueBtn = document.getElementById("continue");
+
+// Card preview text
+const numberPvText = document.getElementById("card-number");
+const holderPvText = document.getElementById("card-holder");
+const expiryPvText = document.getElementById("card-expiry");
+const cvvPvText = document.getElementById("card-cvv");
