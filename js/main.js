d69f43d2b34acd9286c188db1f6705fde0d9afea
nikkehtine <nikkehtine@int.pl>
2023-05-13 19:06:03 +0200

    Break down `listeners` object into three objects

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 import { addZeroes } from "./functions.js";
 
-const listeners = {
-  field: {
-    name: document.getElementById("name"),
-    number: document.getElementById("number"),
-    month: document.getElementById("month"),
-    year: document.getElementById("year"),
-    cvv: document.getElementById("cvv"),
-  },
-  pvText: {
-    number: document.getElementById("card-number"),
-    holder: document.getElementById("card-holder"),
-    expiry: document.getElementById("card-expiry"),
-    cvv: document.getElementById("card-cvv"),
-  },
-  button: {
-    confirm: document.getElementById("confirm"),
-    continue: document.getElementById("continue"),
-  },
+const field = {
+  name: document.getElementById("name"),
+  number: document.getElementById("number"),
+  month: document.getElementById("month"),
+  year: document.getElementById("year"),
+  cvv: document.getElementById("cvv"),
+};
+
+const pvText = {
+  number: document.getElementById("card-number"),
+  holder: document.getElementById("card-holder"),
+  expiry: document.getElementById("card-expiry"),
+  cvv: document.getElementById("card-cvv"),
+};
+
+const button = {
+  confirm: document.getElementById("confirm"),
+  continue: document.getElementById("continue"),
 };
