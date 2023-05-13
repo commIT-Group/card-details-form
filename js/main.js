d307f80f335ca11aab8d0c9d072068b7062f1413
nikkehtine <nikkehtine@int.pl>
2023-05-13 19:58:44 +0200

    Move objects to their own module

--- js/main.js
+++ js/main.js
@@ -1,24 +1,5 @@
 import { addZeroes } from "./functions.js";
-
-const field = {
-  name: document.getElementById("name"),
-  number: document.getElementById("number"),
-  month: document.getElementById("month"),
-  year: document.getElementById("year"),
-  cvv: document.getElementById("cvv"),
-};
-
-const pvText = {
-  number: document.getElementById("card-number"),
-  holder: document.getElementById("card-holder"),
-  expiry: document.getElementById("card-expiry"),
-  cvv: document.getElementById("card-cvv"),
-};
-
-const button = {
-  confirm: document.getElementById("confirm"),
-  continue: document.getElementById("continue"),
-};
+import { field, pvText, button } from "./objects.js";
 
 initListeners();
 
---
+++ js/objects.js
@@ -0,0 +1,21 @@
+export { field, pvText, button };
+
+const field = {
+  name: document.getElementById("name"),
+  number: document.getElementById("number"),
+  month: document.getElementById("month"),
+  year: document.getElementById("year"),
+  cvv: document.getElementById("cvv"),
+};
+
+const pvText = {
+  number: document.getElementById("card-number"),
+  holder: document.getElementById("card-holder"),
+  expiry: document.getElementById("card-expiry"),
+  cvv: document.getElementById("card-cvv"),
+};
+
+const button = {
+  confirm: document.getElementById("confirm"),
+  continue: document.getElementById("continue"),
+};
