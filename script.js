45b6552079f6256f73a0b2c83a2530754bf47f39
nikkehtine <nikkehtine@int.pl>
2023-05-13 17:44:49 +0200

    Use `listeners` object

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-// Form fields
-const nameField = document.getElementById("name");
-const numberField = document.getElementById("number");
-const monthField = document.getElementById("month");
-const yearField = document.getElementById("year");
-const cvvField = document.getElementById("cvv");
-
-// Buttons
-const confirmBtn = document.getElementById("confirm");
-const continueBtn = document.getElementById("continue");
-
-// Card preview text
-const numberPvText = document.getElementById("card-number");
-const holderPvText = document.getElementById("card-holder");
-const expiryPvText = document.getElementById("card-expiry");
-const cvvPvText = document.getElementById("card-cvv");
+const listeners = {
+  field: {
+    name: document.getElementById("name"),
+    number: document.getElementById("number"),
+    month: document.getElementById("month"),
+    year: document.getElementById("year"),
+    cvv: document.getElementById("cvv"),
+  },
+  pvText: {
+    number: document.getElementById("card-number"),
+    holder: document.getElementById("card-holder"),
+    expiry: document.getElementById("card-expiry"),
+    cvv: document.getElementById("card-cvv"),
+  },
+  button: {
+    confirm: document.getElementById("confirm"),
+    continue: document.getElementById("continue"),
+  },
+};
