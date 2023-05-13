12c438d49756110edc8808be9dabb63781c05042
nikkehtine <nikkehtine@int.pl>
2023-05-13 21:10:08 +0200

    Add a function that adds spaces in card number

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,13 @@
-export { addZeroes };
+export { addZeroes, addNumberSpaces };
 
 function addZeroes(value, digits) {
   for (let i = 0; value.length < digits; i++) {
     value = "0" + value;
   }
   return value;
+}
+
+function addNumberSpaces(value) {
+  // Match every 4 digits and join them with a space
+  return value.match(/.{1,4}/g).join(" ");
 }
