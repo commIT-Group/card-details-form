0a01609726f2308868e6b805b7316a1e71382771
nikkehtine <nikkehtine@int.pl>
2023-05-13 18:25:30 +0200

    Add `addZeroes()` function This function adds zeroes in front of a value to make it desired length

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ const listeners = {
     continue: document.getElementById("continue"),
   },
 };
+
+// Function that adds zeroes in front of the value to make it a specified number of digits
+function addZeroes(value, digits) {
+  for (let i = 0; value.length < digits; i++) {
+    value = "0" + value;
+  }
+  return value;
+}
